94df9eb62f8310a7f0655b97e1e51cabff4ed4dd
Oluwamayowa <ademayo234@gmail.com>
2026-05-29 12:06:06 -0700

    added the my shedule menu to the dashboard and the search tutorial menu too

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,13 @@
 import logo from './assets/reekol-logo-bg.png'
 import { Link } from 'react-router-dom';
 import MainDashboardPage from './MainDashboardPage';
+import MyScheduleDashboard from './MyScheduleDashboard';
+import SearchTutorialDashboard from './SearchTutorialDashboard';
 import { useState } from 'react';
 
 
 const dashboard = () => {
-    const [activeTab, setActiveTab] = useState("maindashboardpage")
+    const [activeTab, setActiveTab] = useState("main-dashboard-page")
 
 
     return (
@@ -22,19 +24,19 @@ const dashboard = () => {
                             <div className='grid gap-y-2 my-2'>
                                 <p className='text-gray-600'>MENU</p>
                                 <div className='grid gap-y-2'>
-                                    <div className='flex gap-2 bg-green-50 p-2 rounded-xl text-green'>
+                                    <div onClick={() => setActiveTab("main-dashboard-page")} className={`flex gap-2 ${activeTab === "main-dashboard-page" ? `bg-green-50 text-green` : `bg-white`} p-2 rounded-xl text-gray-600`}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                                         </svg>
                                         <p className='text-[16px]'>Dashboard</p>
                                     </div>
-                                    <div className='flex gap-2 p-2 rounded-xl text-gray-600'>
+                                    <div onClick={() => setActiveTab("search-tutorial-dashboard")} className={`flex gap-2 ${activeTab === "search-tutorial-dashboard" ? `bg-green-50 text-green` : `bg-white`} p-2 rounded-xl text-gray-600`}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                                         </svg>
                                         <p className='text-[16px] '>Search Tutorials</p>
                                     </div>
-                                    <div className='flex gap-2 p-2 rounded-xl text-gray-600'>
+                                    <div onClick={() => setActiveTab("my-schedule-dashboard")} className={`flex gap-2 ${activeTab === "my-schedule-dashboard" ? `bg-green-50 text-green` : `bg-white`} p-2 rounded-xl text-gray-600`}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                                         </svg>
@@ -74,7 +76,9 @@ const dashboard = () => {
                     </div>
                 </div>
                 <div className=''>
-                    {activeTab === "maindashboardpage" && <MainDashboardPage />}
+                    {activeTab === "main-dashboard-page" && <MainDashboardPage />}
+                    {activeTab === "my-schedule-dashboard" && <MyScheduleDashboard />}
+                    {activeTab === "search-tutorial-dashboard" && <SearchTutorialDashboard />}
                 </div>
             </div>
         </div>
---
+++ src/MyScheduleDashboard.jsx
@@ -0,0 +1,119 @@
+const MyScheduleDashboard = () => {
+    const tutorials = [
+        {
+            name: "MTS 202",
+            time: "6:00pm - 8:00pm",
+            location: "FBN LT",
+            tutor: "Bunday",
+            fee: "Free",
+            date: "Today"
+        },
+        {
+            name: "MTS 306",
+            time: "2:00pm - 4:00pm",
+            location: "SOC 3in1 LT",
+            tutor: "Bunday",
+            fee: "Free",
+            date: "Today"
+        },
+        {
+            name: "CHE 102",
+            time: "6:00pm - 8:00pm",
+            location: "ETF",
+            tutor: "Kayshow",
+            fee: "Free",
+            date: "Today"
+        },
+        {
+            name: "MTS 102",
+            time: "7:00pm - 9:00pm",
+            location: "LT 2",
+            tutor: "Machinery",
+            fee: "Free",
+            date: "Today"
+        },
+        {
+            name: "MEE 206",
+            time: "4:00pm - 7:00pm",
+            location: "SEET LR III",
+            tutor: "Samuel",
+            fee: "Free",
+            date: "Tomorrow"
+        },
+    ]
+
+    return (
+        <div className="w-full px-6 pt-6 bg-gray-100 h-full">
+            <div>
+                <div className="flex justify-between items-center">
+                    <p className="text-[40px]">My Schedule</p>
+                    <p className="text-gray-600">Thursday, <span className="text-green">11th of January 2026</span></p>
+                </div>
+                <div className="flex justify-between my-4 items-center">
+                    <select name="" id="" className="bg-gray-0 p-2 rounded-lg">
+                        <option value="this-week">This week</option>
+                        <option value="today">Today</option>
+                        <option value="tomorrow">Tomorrow</option>
+                    </select>
+                    <div className="text-end leading-6">
+                        <p className="text-gray-500 text-[14px] m-0">Now Showing</p>
+                        <p className="text-[20px] m-0">7th of January 2026 - 14th of January 2026</p>
+                    </div>
+                </div>
+                <hr className="text-gray-300" />
+                <div className="grid grid-cols-3 my-4 gap-6">
+                    {tutorials.map((tutorial, index) => (
+                        <div key={index} className="w-88 bg-white px-5  py-2 rounded-2xl">
+                            <div className="flex items-center justify-between">
+                                <p className="text-[35px] font-extrabold">{tutorial.name}</p>
+                                <div className="bg-green-100 flex w-fit rounded-2xl gap-2 items-center px-2 py-1">
+                                    <div className="bg-green h-2 w-2 rounded-full"></div>
+                                    <p>{tutorial.date}</p>
+                                </div>
+                            </div>
+                            <div className="flex gap-2 my-2">
+                                <div className="flex gap-1 items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-500">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                    <p>{tutorial.time}</p>
+                                </div>
+                                <div className="flex gap-1 items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-500">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                                    </svg>
+                                    <p>{tutorial.location}</p>
+                                </div>
+                            </div>
+                            <div className="flex gap-2 my-2">
+                                <div className="flex gap-1 items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-500">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>
+                                    <p>{tutorial.tutor}</p>
+                                </div>
+                                <div className="flex gap-1 items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-500">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                    <p>{tutorial.fee}</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="bg-green text-white flex items-center gap-2 justify-center p-2.5 my-2 rounded-xl">
+                                    <p className="text-[16px]">View Details</p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MyScheduleDashboard;
